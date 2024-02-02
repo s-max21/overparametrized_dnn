632cdec1023abf043cebe2821056c93db32cae31
s-max21 <scharpf.m@outlook.de>
2024-02-02 12:35:52 +0100

    add comments

--- data/data_generator.py
+++ data/data_generator.py
@@ -13,7 +13,6 @@ import tensorflow as tf
 
 # Function to calculate IQR
 def calculate_iqr(data):
-    """Function to calculate the IQR"""
     
     q75, q25 = np.percentile(data, [75, 25])
     return q75 - q25
@@ -35,10 +34,9 @@ def iqr_median(my_func, x_dim=1, num_samples=10**5, num_repetitions=100):
 
 
 # Generate random data samples
-def get_data(my_func, num_samples=10**2, sigma=0.05):
+def get_data(my_func, x_dim=1, num_samples=10**2, sigma=0.05):
     """Function to generate random data samples"""
 
-    x_dim = my_func.expected_dim
     omega = iqr_median(my_func, x_dim=x_dim)
 
     x = np.random.rand(num_samples, x_dim)
