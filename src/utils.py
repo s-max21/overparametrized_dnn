61c798cac1a24b3b7032f2b1b82c4c90f0db83f9
s-max21 <scharpf.m@outlook.de>
2024-02-08 14:51:09 +0100

    tree regression

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
+
+def plot_boxplot(dataframes):
+    fig, ax = plt.subplots()
+    ax.boxplot(dataframes)
+    ax.set_xticklabels(range(1, len(dataframes) + 1))
+    ax.set_xlabel("Model")
+    ax.set_ylabel("Results")
+    ax.set_title("Comparison of Regression Models")
+    plt.show()
