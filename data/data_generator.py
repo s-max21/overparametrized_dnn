5133203e2b84927cac49697d18bdd3b747622edd
s-max21 <scharpf.m@outlook.de>
2024-04-01 19:29:29 +0200

    new seed

--- data/data_generator.py
+++ data/data_generator.py
@@ -14,24 +14,24 @@ def iqr_median(my_func, x_dim=1, num_samples=10**5, num_repetitions=100):
 
     iqr_values = []
     # Generate random samples of x and calculate IQR for each set
-    for _ in range(num_repetitions):
+    for i in range(num_repetitions):
         x_samples = np.random.rand(num_samples, x_dim)
         my_func_values = my_func(x_samples)
         iqr_values.append(calculate_iqr(my_func_values))
-    # Calculate the median of the IQR values
-    median_iqr = np.median(iqr_values)
 
-    return median_iqr
+    return np.median(iqr_values)
 
 
 # Generate random data samples
-def get_data(my_func, x_dim=1, num_samples=10**3, sigma=0.05):
+def get_data(my_func, x_dim=1, num_samples=10**3, sigma=0.05, omega=None, seed=42):
     """Function to generate random data samples"""
+    rng = np.random.default_rng(seed)
 
-    omega = iqr_median(my_func, x_dim=x_dim)
+    if omega is None:
+        omega = iqr_median(my_func, x_dim=x_dim)
 
-    x = np.random.rand(num_samples, x_dim)
-    y = my_func(x) + sigma * omega * np.random.normal(0, 1, (num_samples,))
+    x = rng.random(size=(num_samples, x_dim))
+    y = my_func(x) + sigma * omega * rng.standard_normal(size=num_samples)
 
     return (x, y)
 
@@ -45,7 +45,6 @@ def preprocess(x, y, batch_size=64, training=False):
     if training:
         data = data.shuffle(buffer_size=len(x))
 
-    data = data.batch(batch_size)
-    data = data.prefetch(tf.data.AUTOTUNE)
+    data = data.batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
     return data
